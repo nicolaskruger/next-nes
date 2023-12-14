4e0ab51b5659a8f29ae50e152ac0897a1e247d77
nkruger <nicolas.nkruger@gmail.com>
2023-12-14 10:27:16 -0300

    feat: :construction: compiler in preogress...

--- src/nes/cpu/compiler/compiler.spec.ts
+++ src/nes/cpu/compiler/compiler.spec.ts
@@ -1,4 +1,4 @@
-import { breakInstruction } from "./compiler";
+import { breakInstruction, compile } from "./compiler";
 
 const program = `
     LSR A
@@ -6,10 +6,84 @@ const program = `
     LDA #10 
 `;
 
+const allAddrProgram = `
+    CLC
+    LSR A
+    LDA #10
+    LDA $00
+    STY $10,X
+    LDX $10,Y
+    BNE *+4
+    JMP $1234
+    STA $3000,X
+    AND $4000,Y
+    JMP ($FFFC)
+    LDA ($40,X)
+    LDA ($40),Y
+`;
+
+const invalidOpcodeProgram = `
+    LSR $1234
+`;
+
+const errorInstructionNotFoundProgram = `
+    DEL
+`;
+
+const errorAddrNotExistsProgram = `
+    LSA NOT
+`;
+
 describe("compiler", () => {
   test("break instruction", () => {
     const instruction = breakInstruction(program);
 
     expect(instruction).toStrictEqual(["LSR", "A", "ROR", "A", "LDA", "#10"]);
+  });
+
+  test.skip("compile", () => {
+    const comp = compile(allAddrProgram);
+
+    const CLC = [0x18];
+    const LSR_A = [0x4a];
+    const LDA_IMM = [0xa9, 10];
+    const LDA_$ = [0xa5, 0x00];
+    const STY_$X = [0x94, 0x10];
+    const LDX_$Y = [0xb6, 0x10];
+    const BNE = [0xd0, 4];
+    const JMP_$ = [0x4c, 0x34, 0x12];
+    const STA_X = [0x9d, 0x00, 0x30];
+    const AND_Y = [0x39, 0x00, 0x40];
+    const JMP_IND = [0x6c, 0xfc, 0xff];
+    const LDA_IND_X = [0xa1, 0x40];
+    const LDA_IND_Y = [0xb1, 0x40];
+
+    expect(comp).toStrictEqual([
+      ...CLC,
+      ...LSR_A,
+      ...LDA_IMM,
+      ...LDA_$,
+      ...STY_$X,
+      ...LDX_$Y,
+      ...BNE,
+      ...JMP_$,
+      ...STA_X,
+      ...AND_Y,
+      ...JMP_IND,
+      ...LDA_IND_X,
+      ...LDA_IND_Y,
+    ]);
+  });
+
+  test.skip("instruction not found", () => {
+    expect(() => compile(errorInstructionNotFoundProgram)).toThrow(
+      "instruction DEL not exists"
+    );
+  });
+
+  test.skip("addr not found", () => {
+    expect(() => compile(errorAddrNotExistsProgram)).toThrow(
+      "addr NOT is invalid"
+    );
   });
 });
--- src/nes/cpu/compiler/compiler.ts
+++ src/nes/cpu/compiler/compiler.ts
@@ -1,4 +1,191 @@
+import { Dictionary } from "@/nes/helper/dictionary";
+import { instructionDictionary } from "../intructionDictionary/instructionDictionary";
+
+type ADDR =
+  | "IMP"
+  | "ACC"
+  | "IMM"
+  | "ZERO_PAGE"
+  | "ZERO_PAGE_X"
+  | "ZERO_PAGE_Y"
+  | "RELATIVE"
+  | "ABSOLUTE"
+  | "ABSOLUTE_X"
+  | "ABSOLUTE_Y"
+  | "INDIRECT"
+  | "INDEXED_INDIRECT"
+  | "INDIRECT_INDEXED";
+
+type INSTRUCTION =
+  | "ADC"
+  | "AND"
+  | "ASL"
+  | "BCC"
+  | "BCS"
+  | "BEQ"
+  | "BIT"
+  | "BMI"
+  | "BNE"
+  | "BPL"
+  | "BRK"
+  | "BVC"
+  | "BVS"
+  | "CLC"
+  | "CLD"
+  | "CLI"
+  | "CLV"
+  | "CMP"
+  | "CPX"
+  | "CPY"
+  | "DEC"
+  | "DEY"
+  | "EOR"
+  | "INC"
+  | "INX"
+  | "INY"
+  | "JMP"
+  | "JSR"
+  | "LDA"
+  | "LDX"
+  | "LDY"
+  | "LSR"
+  | "NOP"
+  | "ORA"
+  | "PHA"
+  | "PHP"
+  | "PLA"
+  | "PLP"
+  | "ROL"
+  | "ROR"
+  | "RTI"
+  | "RTS"
+  | "SBC"
+  | "SEC"
+  | "SED"
+  | "SEI"
+  | "STA"
+  | "STX"
+  | "STY"
+  | "TAX"
+  | "TAY"
+  | "TSX"
+  | "TXA"
+  | "TXS"
+  | "TYA";
+
+type Compiler = {
+  comp: number[];
+  instruction?: INSTRUCTION;
+};
+
+const instructionDictionaryOpcode: Dictionary<
+  INSTRUCTION,
+  Partial<Dictionary<ADDR, { opcode: number }>>
+> = instructionDictionaryBuilder();
+
+const addrDataDictionary: Dictionary<
+  ADDR,
+  { data: (value: string) => number[] }
+> = {
+  IMM: {
+    data: (value) => [],
+  },
+};
+
+function instructionDictionaryBuilder() {
+  return Object.keys(instructionDictionary).reduce((acc, curr) => {
+    const opcode = curr;
+    const info = instructionDictionary[Number(curr)];
+    const instruction = functionToInstruction(info.instruction);
+    const addr = functionToAddr(info.addr);
+
+    return {
+      ...acc,
+      [instruction]: {
+        ...acc[instruction],
+        [addr]: {
+          opcode,
+        },
+      },
+    };
+  }, {} as Dictionary<INSTRUCTION, Partial<Dictionary<ADDR, { opcode: number }>>>);
+}
+
+const isImpliedInstruction = (instruction: INSTRUCTION): boolean => {
+  throw new Error("not implemented");
+};
+
+const stringToInstruction = (instruction: string): INSTRUCTION => {
+  throw new Error("not implemented");
+};
+
+const isACC = (addr: string) => addr === "A";
+const isIMM = (addr: string) => /#[]/;
+
+const stringToAddr = (instruction: string): ADDR => {
+  throw new Error("not implemented");
+};
+
+function functionToInstruction(func: Function): INSTRUCTION {
+  return stringToInstruction(func.name);
+}
+
+function functionToAddr(func: Function): ADDR {
+  return stringToAddr(func.name);
+}
+
 const breakInstruction = (program: string): string[] =>
   program.split(/\s+/).filter((v) => v);
 
-export { breakInstruction };
+const fetchOpcode = (instruction: INSTRUCTION, addr: ADDR): number => {
+  const addrDictionary = instructionDictionaryOpcode[instruction];
+  const opcode = addrDictionary[addr];
+  if (opcode == undefined) throw new Error("invalid opcode");
+  return opcode.opcode;
+};
+
+const instructionFlow = (acc: Compiler, curr: string): Compiler => {
+  const currInstruction = stringToInstruction(curr);
+  if (isImpliedInstruction(currInstruction)) {
+    const opcode = fetchOpcode(currInstruction, "IMP");
+    return {
+      ...acc,
+      comp: [...acc.comp, opcode],
+      instruction: undefined,
+    };
+  }
+  return {
+    ...acc,
+    instruction: currInstruction,
+  };
+};
+
+const opcodeFlow = (acc: Required<Compiler>, curr: string): Compiler => {
+  const { instruction } = acc;
+  const addr = stringToAddr(curr);
+  const opcode = fetchOpcode(instruction, addr);
+  const data = addrDataDictionary[addr].data(curr);
+
+  return {
+    ...acc,
+    instruction: undefined,
+    comp: [...acc.comp, opcode, ...data],
+  };
+};
+
+const compile = (program: string): number[] => {
+  const instruction = breakInstruction(program);
+
+  instruction.reduce(
+    (acc, curr) => {
+      const { instruction } = acc;
+      if (instruction == undefined) return instructionFlow(acc, curr);
+      else return opcodeFlow({ ...acc, instruction }, curr);
+    },
+    { comp: [] } as Compiler
+  );
+
+  return [];
+};
+
+export { breakInstruction, compile };
