c4aa53126ce135d68ab9ca2a35f9a4263fa7fa0b
nkruger <nicolas.nkruger@gmail.com>
2024-01-16 16:44:23 -0300

    feat: :sparkles: writeSprRam

--- src/nes/ppu/spr-ram/spr-ram.spec.ts
+++ src/nes/ppu/spr-ram/spr-ram.spec.ts
@@ -1,8 +1,8 @@
 import { initNes } from "@/nes/nes";
-import { readSprRam } from "./spr-ram";
+import { readSprRam, writeSprRam } from "./spr-ram";
 
 describe("spr-ram", () => {
-  test("should not access memory less than 0x00 nether mote 0xff", () => {
+  test("should not read memory less than 0x00 nether mote 0xff", () => {
     let nes = initNes();
 
     expect(() => readSprRam(-1, nes)).toThrow("PPU SPR-RAM out of range");
@@ -13,6 +13,23 @@ describe("spr-ram", () => {
     let nes = initNes();
 
     nes.ppu.sprRam[0].data = 4;
+
+    expect(readSprRam(0, nes)).toBe(4);
+  });
+
+  test("should not write memory less than 0x00 nether mote 0xff", () => {
+    let nes = initNes();
+
+    expect(() => writeSprRam(-1, 1, nes)).toThrow("PPU SPR-RAM out of range");
+    expect(() => writeSprRam(0x100, 1, nes)).toThrow(
+      "PPU SPR-RAM out of range"
+    );
+  });
+
+  test("should write memory", () => {
+    let nes = initNes();
+
+    nes = writeSprRam(0x00, 4, nes);
 
     expect(readSprRam(0, nes)).toBe(4);
   });
--- src/nes/ppu/spr-ram/spr-ram.ts
+++ src/nes/ppu/spr-ram/spr-ram.ts
@@ -5,8 +5,19 @@ const getSprRam = (nes: Nes): Bus => nes.ppu.sprRam;
 
 const simpleReadSprRam = (addr: number, nes: Nes) => getSprRam(nes)[addr].data;
 
+const simpleWriteSprRam = (addr: number, value: number, nes: Nes): Nes => {
+  getSprRam(nes)[addr].data = value;
+  return nes;
+};
+
 export const readSprRam = (addr: number, nes: Nes): number => {
   if (addr >= 0 && addr <= 0xff) return getSprRam(nes)[addr].read(addr, nes);
+  throw new Error("PPU SPR-RAM out of range");
+};
+
+export const writeSprRam = (addr: number, value: number, nes: Nes): Nes => {
+  if (addr >= 0 && addr <= 0xff)
+    return getSprRam(nes)[addr].write(addr, value, nes);
   throw new Error("PPU SPR-RAM out of range");
 };
 
@@ -14,4 +25,8 @@ export const initSprRam = (): Bus =>
   "_"
     .repeat(0x100)
     .split("")
-    .map((_) => ({ data: 0, read: simpleReadSprRam, write: simpleWrite }));
+    .map((_) => ({
+      data: 0,
+      read: simpleReadSprRam,
+      write: simpleWriteSprRam,
+    }));
