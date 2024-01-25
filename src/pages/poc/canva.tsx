834f1ac8344c9dc28a69d26dd8b06c1ceb6c4c3c
nkruger <nicolas.nkruger@gmail.com>
2024-01-25 14:16:04 -0300

    feat: :sparkles: canvas

--- src/pages/poc/canva.tsx
+++ src/pages/poc/canva.tsx
@@ -1,5 +1,5 @@
 import { COLOR_PALLET, colorToHex } from "@/nes/ppu/color/color";
-import { RefObject, useEffect, useRef } from "react";
+import { RefObject, useCallback, useEffect, useRef } from "react";
 
 type CanvasRef = RefObject<HTMLCanvasElement>;
 
@@ -11,14 +11,18 @@ const getCanvasContext = (ref: CanvasRef) =>
 export default function Page() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  useEffect(() => {
+  const draw = useCallback(() => {
     for (let i = 0; i < 256 * 240; i++) {
       getCanvasContext(canvasRef).fillStyle = colorToHex(
         Math.floor(Math.random() * COLOR_PALLET.length)
       );
       getCanvasContext(canvasRef)?.fillRect(i % 256, Math.floor(i / 256), 1, 1);
     }
-  }, []);
+  }, [canvasRef]);
+  useEffect(() => {
+    const id = setInterval(() => draw(), 1);
+    return () => clearInterval(id);
+  }, [draw]);
 
   return <canvas ref={canvasRef} width={256} height={240} />;
 }
