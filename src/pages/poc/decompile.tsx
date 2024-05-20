aba05046adc4501fc1d387e671e25847dc2086bd
nkruger <nicolas.nkruger@gmail.com>
2024-05-20 14:58:02 -0300

    feat: :sparkles: decompile screen

---
+++ src/pages/poc/decompile.tsx
@@ -0,0 +1,11 @@
+export default function Decompile() {
+  return (
+    <main className="flex w-screen h-screen">
+      <div className="w-1/3 flex-col flex">
+        <div className="w-full h-2/3 bg-blue-500"></div>
+        <div className="w-full h-1/3 bg-red-500"></div>
+      </div>
+      <div className="w-2/3 bg-purple-500"></div>
+    </main>
+  );
+}
